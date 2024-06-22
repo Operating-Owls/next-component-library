cdac1fc168aa8b94c9fbee4e4d09f782261f333e
Benjamin Schoolland <bschoolland@gmail.com>
2024-06-22 11:19:47 -0700

    feat: create basic home page html

--- app/page.js
+++ app/page.js
@@ -1,8 +1,28 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>TODO: add links to the component showcases</p>
+      <h1>Welcome to the Bay Valley Tech Component Library</h1>
+      <p>
+        This is a collection of components built by interns at Bay Valley Tech.
+      </p>
+      <h2>List of components:</h2>
+      <ul>
+        <li>
+          <h3>Toast</h3>
+          <p>
+            By Benjamin Schoolland
+          </p>
+          <p>
+            A simple toast notification component, for displaying messages to the user.
+          </p>
+          <p>
+            <a href="/toast-demo">View Demo</a>
+          </p>
+        </li>
+        <li>
+          <p>TODO: add more</p>
+        </li>
+      </ul>
     </div>
   );
 }
